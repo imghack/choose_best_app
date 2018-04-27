7724fdd9c3d5da7615fed19d37660e84f1a8df14
Maxim Romaniv <Maxim.Romaniv@netent.com>
2018-04-27 22:22:17 +0300

    [preprocessing] added basic preprocessing

--- src/data/data_preprocessing.py
+++ src/data/data_preprocessing.py
@@ -1,31 +1,32 @@
 import csv
+from utils.time_utils import append_time_duration
 
 READ_ROOT_FOLDER = '../../data/raw/'
 WRITE_ROOT_FOLDER = '../../data/processed/'
 FILES = ['apps.csv', 'link_data.csv', 'orders.csv']
 
-docs = {}
-
 
 def read():
+    documents = {}
     for file_name in FILES:
         file_path = READ_ROOT_FOLDER + file_name
         with open(file_path, 'r') as f:
             reader = csv.reader(f)
-            docs[file_name] = list(reader)
-    return docs
+            documents[file_name] = list(reader)
+    return documents
 
 
-def write():
+def write(documents):
     for file_name in FILES:
         file_path = WRITE_ROOT_FOLDER + file_name
 
         file = open(file_path, 'w')
         with file:
             writer = csv.writer(file)
-            writer.writerows(docs[file_name])
+            writer.writerows(documents[file_name])
 
 
 if __name__ == '__main__':
-    read()
-    write()
+    docs = read()
+    append_time_duration(docs['apps.csv'], start_time_index=1, end_time_index=2)
+    write(docs)
