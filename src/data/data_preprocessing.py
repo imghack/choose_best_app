29188555784e7de4c5b07b02585ccb583baccc73
Maxim Romaniv <Maxim.Romaniv@netent.com>
2018-05-01 10:11:08 +0300

    [data_preprocessing] added

--- src/data/data_preprocessing.py
+++ src/data/data_preprocessing.py
@@ -3,30 +3,65 @@ from utils.time_utils import append_time_duration
 
 READ_ROOT_FOLDER = '../../data/raw/'
 WRITE_ROOT_FOLDER = '../../data/processed/'
-FILES = ['apps.csv', 'link_data.csv', 'orders.csv']
+FILES = ['apps.csv']
+SUB_FILES = ['link_data.csv', 'orders.csv']
 
 
 def read():
-    documents = {}
-    for file_name in FILES:
-        file_path = READ_ROOT_FOLDER + file_name
-        with open(file_path, 'r') as f:
-            reader = csv.reader(f)
-            documents[file_name] = list(reader)
-    return documents
+    apps = []
+    link_data = {}
+    orders = {}
+
+    with open(READ_ROOT_FOLDER + 'link_data.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            link_data[row['ApplicationID'].lower()] = row
+
+    with open(READ_ROOT_FOLDER + 'orders.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            orders[row['SessionId'].lower()] = row
+
+    with open(READ_ROOT_FOLDER + 'apps.csv', 'r') as f:
+        reader = csv.reader(f)
+        apps = list(reader)
+
+    return apps, link_data, orders
 
 
-def write(documents):
-    for file_name in FILES:
-        file_path = WRITE_ROOT_FOLDER + file_name
+def write(apps):
+    file = open(WRITE_ROOT_FOLDER + 'apps.csv', 'w')
+    with file:
+        writer = csv.writer(file)
+        writer.writerows(apps)
 
-        file = open(file_path, 'w')
-        with file:
-            writer = csv.writer(file)
-            writer.writerows(documents[file_name])
+
+# TODO: you can have few session id's with difference revenue ... add time to get correct revenue
+def append_revenue(apps, link_data, orders):
+    apps[0].append('Revenue')
+
+    counter = 0
+
+    for app in apps[1:]:
+        try:
+            link = link_data[app[3]]
+            sess_id = link['SessionID'].lower()
+            revenue = orders[sess_id]['Revenue']
+            app.append(round(float(revenue), 2))
+        except:
+            apps.remove(app)
+            counter += 1
+
+    print('MISSED ITEMS ', counter)
 
 
 if __name__ == '__main__':
-    docs = read()
-    append_time_duration(docs['apps.csv'], start_time_index=1, end_time_index=2)
-    write(docs)
+    apps, link_data, orders = read()
+
+    print(len(apps))
+    print(len(link_data.keys()))
+    print(len(orders.keys()))
+    append_time_duration(apps, start_time_index=1, end_time_index=2)
+    append_revenue(apps, link_data, orders)
+
+    write(apps)
--- src/visualisation/visualize.py
+++ src/visualisation/visualize.py
@@ -4,19 +4,34 @@ import matplotlib.pyplot as plt
 
 data = defaultdict(list)
 
-with open('../../data/processed/apps.csv', 'r') as f:
-    reader = csv.DictReader(f)  # read rows into a dictionary format
-    for row in reader:  # read a row as {column1: value1, column2: value2,...}
-        for (k, v) in row.items():  # go over each column name and value
-            if k == 'Time' or k == 'Duration':
+PATH = '../../data/processed/apps.csv'
+
+with open(PATH, 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        for (k, v) in row.items():
+            if k == 'Time' or \
+                    k == 'Duration' or \
+                    k == 'Revenue':
                 v = float(v)
             data[k].append(v)
 
 fig, ax = plt.subplots()
-count = 600
 ax.set_ylabel('duration')
 ax.set_xlabel('hours')
 ax.plot(data['Time'], data['Duration'], 'o')
+
+count = 600
 ax.plot(data['Time'][:count], data['Duration'][:count], 'o')
 ax.set_title('Duration when people are using apps')
+
+fig1, ax1 = plt.subplots()
+ax1.set_ylabel('revenue')
+ax1.set_xlabel('hours')
+ax1.plot(data['Time'], data['Revenue'], 'o')
+
+count = 600
+ax1.plot(data['Time'][:count], data['Revenue'][:count], 'o')
+ax1.set_title('Revenue')
+
 plt.show()
