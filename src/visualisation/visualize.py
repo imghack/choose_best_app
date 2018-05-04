26de659db2274eea7c09085b85b73bd942cd4cf4
Maxim Romaniv <Maxim.Romaniv@netent.com>
2018-05-04 15:45:08 +0300

    [visualize] added graph for 25 times less random apps

--- src/visualisation/visualize.py
+++ src/visualisation/visualize.py
@@ -22,19 +22,34 @@ def fetch_data(path):
     return data
 
 
-all_apps_path = ''.join([PATH, 'apps.csv'])
-data = fetch_data(all_apps_path)
-# show all and random and random 600 apps
-fig, ax = plt.subplots()
-ax.plot(data['Time'], data['Duration'], 'o')
-count = 600
-ax.plot(data['Time'][:count], data['Duration'][:count], 'o')
-
-# show all and random and random 600 apps
-fig, ax = plt.subplots()
-ax.plot(data['Time'], data['Revenue'], 'o')
-count = 600
-ax.plot(data['Time'][:count], data['Revenue'][:count], 'o')
+# all_apps_path = ''.join([PATH, 'apps.csv'])
+# data = fetch_data(all_apps_path)
+# # show all and random and random 600 apps
+# fig, ax = plt.subplots()
+# ax.plot(data['Time'], data['Duration'], 'o')
+# count = 600
+# ax.plot(data['Time'][:count], data['Duration'][:count], 'o')
+#
+# # show all and random and random 600 apps
+# fig, ax = plt.subplots()
+# ax.plot(data['Time'], data['Revenue'], 'o')
+# count = 600
+# ax.plot(data['Time'][:count], data['Revenue'][:count], 'o')
+#
+# # show apps by app name
+# fig, ax = plt.subplots()
+# files = os.listdir(APPS_PATH)
+# for file in files:
+#     app_path = ''.join([APPS_PATH, file])
+#     data = fetch_data(app_path)
+#     ax.plot(data['Time'], data['Duration'], 'o')
+#
+# fig, ax = plt.subplots()
+# files = os.listdir(APPS_PATH)
+# for file in files:
+#     app_path = ''.join([APPS_PATH, file])
+#     data = fetch_data(app_path)
+#     ax.plot(data['Time'], data['Revenue'], 'o')
 
 # show apps by app name
 fig, ax = plt.subplots()
@@ -42,13 +57,16 @@ files = os.listdir(APPS_PATH)
 for file in files:
     app_path = ''.join([APPS_PATH, file])
     data = fetch_data(app_path)
-    ax.plot(data['Time'], data['Duration'], 'o')
+    size = int(len(data['Time']) / 25)
+    ax.plot(data['Time'][:size], data['Duration'][:size], 'o')
+
 
 fig, ax = plt.subplots()
 files = os.listdir(APPS_PATH)
 for file in files:
     app_path = ''.join([APPS_PATH, file])
     data = fetch_data(app_path)
-    ax.plot(data['Time'], data['Revenue'], 'o')
+    size = int(len(data['Time']) / 25)
+    ax.plot(data['Time'][:size], data['Revenue'][:size], 'o')
 
 plt.show()
